178aa43406c20687ab9643c3e125e99d9bde9542
marc <67497156+marcmyworld@users.noreply.github.com>
2026-09-08 19:14:36 +0530

    Update build script for Bliss ROMs

---
+++ build/crave/android16-qpr2/build_blissroms.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+banner() {
+    clear
+
+    echo -e "${CYAN}${BOLD}"
+    echo "╔════════════════════════════════════════════════════════════════════════════╗"
+    echo "║                                                                            ║"
+    echo "║   ██████╗ ██╗     ██╗███████╗███████╗██████╗  ██████╗ ███╗   ███╗███████╗  ║"
+    echo "║   ██╔══██╗██║     ██║██╔════╝██╔════╝██╔══██╗██╔═══██╗████╗ ████║██╔════╝  ║"
+    echo "║   ██████╔╝██║     ██║███████╗███████╗██████╔╝██║   ██║██╔████╔██║███████╗  ║"
+    echo "║   ██╔══██╗██║     ██║╚════██║╚════██║██╔══██╗██║   ██║██║╚██╔╝██║╚════██║  ║"
+    echo "║   ██████╔╝███████╗██║███████║███████║██║  ██║╚██████╔╝██║ ╚═╝ ██║███████║  ║"
+    echo "║   ╚═════╝ ╚══════╝╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝  ║"
+    echo "║                                                                            ║"
+    echo "║                            B L I S S R O M S                               ║"
+    echo "║                         Automated Release Builder                          ║"
+    echo "║                                                                            ║"
+    echo "╠════════════════════════════════════════════════════════════════════════════╣"
+    echo "║  Build      : bp4a-user                                                    ║"
+    echo "║  Branch     : waterlily                                                    ║"
+    echo "╚════════════════════════════════════════════════════════════════════════════╝"
+    echo -e "${RESET}"
+}
+
+
+
+banner;
+
+echo "============================================="
+echo "    cleaning up previous local manifests    "
+echo "============================================="
+
+rm -rf .repo/local_manifests;
+rm -rf out/soong/.intermediates/system/sepolicy;
+
+
+echo ""
+echo "====================="
+echo "      repo init      "
+echo "====================="
+
+repo init -u https://github.com/BlissRoms/stable_releases.git -b waterlily --depth=1 --git-lfs;
+git clone https://github.com/marcmyworld/chenfeng_manifest.git -b lineage-23.2 --depth=1 .repo/local_manifests;
+
+
+echo ""
+echo "==================="
+echo "     repo sync     "
+echo "==================="
+
+/opt/crave/resync.sh;
+
+export BUILD_USERNAME=marc
+export BUILD_HOSTNAME=foss
+
+rm -rf build/soong/fsgen;
+
+
+echo ""
+echo "===================="
+echo "   starting build  "
+echo "===================="
+
+source build/envsetup.sh;
+export WITH_GMS=true
+export BLISS_BUILD_VARIANT="GAPPS"
+blissify chenfeng;
+
+
+echo ""
+echo "=========================================="
+echo "      uploading to sharing platforms      "
+echo "=========================================="
+
+ZIP=$(find out/target/product/chenfeng -maxdepth 1 -type f -name "*.zip" | head -n 1)
+
+if [ -n "$ZIP" ]; then
+    echo "Uploading: $ZIP..."
+    wget https://raw.githubusercontent.com/marcmyworld/chenfeng/refs/heads/main/tools/upload_util.sh
+    chmod +x upload_util.sh
+    ./upload_util.sh "$ZIP"
+else
+    echo "No ROM ZIP found in artifacts!"
+    exit 1
+fi
