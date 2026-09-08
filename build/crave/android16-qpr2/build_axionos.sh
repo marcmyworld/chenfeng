d7649a544a6ca55ead51eed1919bc960271bc10e
marc <67497156+marcmyworld@users.noreply.github.com>
2026-09-08 23:04:21 +0530

    Add build script for Axion OS automation
    This script automates the build process for Axion OS, including initialization, syncing, and uploading the build artifacts.

---
+++ build/crave/android16-qpr2/build_axionos.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+banner() {
+    clear
+    echo -e "${CYAN}${BOLD}"
+    echo "╔═════════════════════════════════════════════════════════════╗"
+    echo "║                                                             ║"
+    echo "║    █████╗ ██╗  ██╗██╗ ██████╗ ███╗   ██╗     ██████╗ ████╗  ║"
+    echo "║   ██╔══██╗╚██╗██╔╝██║██╔═══██╗████╗  ██║    ██╔═══██╗██╔═╝  ║"
+    echo "║   ███████║ ╚███╔╝ ██║██║   ██║██╔██╗ ██║    ██║   ██║████╗  ║"
+    echo "║   ██╔══██║ ██╔██╗ ██║██║   ██║██║╚██╗██║    ██║   ██║╚═██║  ║"
+    echo "║   ██║  ██║██╔╝ ██╗██║╚██████╔╝██║ ╚████║    ╚██████╔╝████║  ║"
+    echo "║   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝     ╚═════╝ ╚═══╝  ║"
+    echo "║                                                             ║"
+    echo "║              A X I O N   O S                                ║"
+    echo "║              Automated Release Builder                      ║"
+    echo "║                                                             ║"
+    echo "╠═════════════════════════════════════════════════════════════╣"
+    echo "║  Build      : bp4a-user                                     ║"
+    echo "║  Branch     : lineage-23.2                                  ║"
+    echo "╚═════════════════════════════════════════════════════════════╝"
+    echo -e "${RESET}"
+}
+
+
+
+banner;
+
+echo "============================================="
+echo "    cleaning up previous local manifests    "
+echo "============================================="
+
+rm -rf .repo/local_manifests;
+rm -rf out/soong/.intermediates/system/sepolicy;
+
+
+echo ""
+echo "====================="
+echo "      repo init      "
+echo "====================="
+
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --depth=1 --git-lfs;
+git clone https://github.com/marcmyworld/chenfeng_manifest.git -b lineage-23.2 --depth=1 .repo/local_manifests;
+
+
+echo ""
+echo "==================="
+echo "     repo sync     "
+echo "==================="
+
+/opt/crave/resync.sh;
+
+export BUILD_USERNAME=marc
+export BUILD_HOSTNAME=foss
+
+rm -rf build/soong/fsgen;
+
+
+echo ""
+echo "===================="
+echo "   starting build  "
+echo "===================="
+
+. build/envsetup.sh;
+
+export WITH_GMS=true
+export TARGET_DISABLE_EPPE=true
+export TARGET_INCLUDE_AXFX=true
+export AXION_CAMERA_REAR_INFO="50,50,12"
+export AXION_CAMERA_FRONT_INFO="32,32"
+export AXION_MAINTAINER="xeon_man"
+export AXION_PROCESSOR="Snapdragon_8s_Gen_3"
+export TARGET_INCLUDE_GOOGLE_TELECOMM=false
+export TARGET_INCLUDE_PARTNER_SETUP=true
+
+axion chenfeng user gms;
+m pixelos -j$(nproc --all);
+
+
+echo ""
+echo "=========================================="
+echo "      uploading to sharing platforms      "
+echo "=========================================="
+
+ZIP=$(find out/target/product/chenfeng -maxdepth 1 -type f -name "*.zip" | head -n 1)
+
+if [ -n "$ZIP" ]; then
+    echo "Uploading: $ZIP..."
+    wget https://raw.githubusercontent.com/marcmyworld/chenfeng/refs/heads/main/tools/upload_util.sh
+    chmod +x upload_util.sh
+    ./upload_util.sh "$ZIP"
+else
+    echo "No ROM ZIP found in artifacts!"
+    exit 1
+fi
